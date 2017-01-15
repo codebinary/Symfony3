3ea254017ff828dcc3ea6770d59237f74aab5035
James Ontiveros <james.ontiveros86@gmail.com>
2017-01-15 17:49:07 -0500

    Ruta y acción para el login y validar un email

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class DefaultController extends Controller
 {
     
@@ -15,6 +17,41 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+
+    public function loginAction(Request $request){
+        $helpers = $this->get("app.helpers");
+
+        //Vamos a recibir un json por post
+        $json = $request->get("json", null);
+
+        if ($json != null) {
+            //Recogemos los parámetros y decodificamos
+            $params = json_decode($json);
+
+            //Obtenemos los valores email y password
+            $email = (isset($params->email) ? $params->email : null);
+            $password = (isset($params->password) ? $params->password : null);
+
+            //Restricción de email
+            $emailContraint = new Assert\Email();
+            $emailContraint->message = "This email is not valid !!";
+
+            //Validamos los parámetros
+            $validate_email = $this->get("validator")->validate($email, $emailContraint);
+
+            //Si hay datos de error en $validate_email entonces es invalido
+            if (count($validate_email) == 0 && $password != null) {
+                echo "Data Success !!";
+            }else{
+                echo "Data incorrent !!";
+            }
+
+
+        }else{
+            echo "No hay datos";
+            die();
+        }
     }
 
 
