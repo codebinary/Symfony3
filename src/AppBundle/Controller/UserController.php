4187f719807f32694620a0db27ea41569c19e420
James Ontiveros <james.ontiveros86@gmail.com>
2017-01-16 13:21:50 -0500

    Implementando la edición de Usuarios

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -92,4 +92,112 @@ class UserController extends Controller{
 
 	}
 
+
+
+
+	//Método para editar un usuario
+	public function editAction(Request $request){
+
+		//Cargamos el servicio de helpers
+		$helpers = $this->get("app.helpers");
+
+		//Comprabamos que el token que llega sea el correcto
+		$hash = $request->get("authorization", null);
+		$authCheck = $helpers->authCheck($hash);
+
+		//Si el token es correcto entonces realizamos todo el proceso
+		if ($authCheck == true) {
+			
+			//Decodificamos los datos que lleguen en el token
+			$identity = $helpers->authCheck($hash, true);
+
+			$em = $this->getDoctrine()->getManager();
+			//Igualamos el valor del token llamando sub con el id si coinciden
+			$user = $em->getRepository("BackendBundle:User")->findOneBy(
+					array(
+							"id" => $identity->sub
+						)
+				);
+
+			//Recogemos los datos que llegan de post
+			$json = $request->get("json", null);
+			//Parámetros
+			$params = json_decode($json);
+
+			$data = array(
+				"status" => "error",
+				"code" => 400,
+				"msg" => "User not updated"
+			);
+
+			if ($json != null) {
+
+				$createdAt = new \DateTime("now");
+				$image = null;
+				$role = "user";
+
+				$email = (isset($params->email)) ? $params->email : null;
+				$name = (isset($params->name) && ctype_alpha($params->name)) ? $params->name : null;
+				$surname = (isset($params->surname) && ctype_alpha($params->surname)) ? $params->surname : null;
+				$password = (isset($params->password)) ? $params->password : null;
+
+				$emailContraint = new Assert\Email();
+	            $emailContraint->message = "This email is not valid !!";
+	            $validate_email = $this->get("validator")->validate($email, $emailContraint);
+
+				if ($email != null && count($validate_email) == 0 && $name != null && $surname != null) {
+					
+					//Creamos el usuarios
+					//$user = new User();
+
+					$user->setCreatedAt($createdAt);
+					$user->setImage($image);
+					$user->setRole($role);
+					$user->setEmail($email);
+					$user->setName($name);
+					$user->setSurname($surname);
+
+					if ($password != null) {
+						//Cifrar password
+						$pwd = hash('sha256', $password);
+						$user->setPassword($pwd);
+					}
+
+					$em = $this->getDoctrine()->getManager();
+
+					//Buscamos el email para saber que no está y luego guardarlo
+					$isset_user = $em->getRepository("BackendBundle:User")->findBy(
+							array(
+								"email" => $email
+							)
+						);
+
+					//confirmamos si el email es igual al que recibimos por post
+					if (count($isset_user) == 0 || $identity->email == $email) {
+						$em->persist($user); //Persistimos en la clase los datos
+						$em->flush(); //Guardamos en la base de datos
+
+						$data["status"] = 'success';
+						$data["code"] = 200;
+						$data["msg"]	= 'User updated created!!';
+					}else{
+						$data = array(
+							"status" 	=> "error",
+							"code"		=>	400,
+							"msg"		=> "User not updated, duplicated!!"
+						);
+					}
+				}
+			}
+		}else{
+			$data = array(
+					"status" 	=> "error",
+					"code"		=> 	400,
+					"msg"		=> 	"Authorization not valid"
+				);
+		}
+
+	return $helpers->json($data);
+	}
+
 }
